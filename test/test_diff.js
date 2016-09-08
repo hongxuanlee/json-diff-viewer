8fb8da913c110cba512969be6f26abfac781b8c9
hongxuanlee <lihongxuan213@gmail.com>
2016-09-08 21:38:59 +0800

    add test

--- test/test_diff.js
+++ test/test_diff.js
@@ -3,11 +3,37 @@ const diff = require('../src/diff');
 
 describe('diff json', () => {
     it('should merge 2 number', () => {
-        let x = 10;
-        let y = 11;
         let {merge, diffMap} = diff(10, 11);
         expect(merge).to.equal(10);
         expect(diffMap).to.deep.equal({type: 'edit', modify: 11 });
+    });
+    it('should merge number and object', () => {
+        let x = {x: 1};
+        let y = 11;
+        let {merge, diffMap} = diff(x, y);
+        expect(merge).to.deep.equal({x: 1});
+        expect(diffMap).to.deep.equal({type: 'edit', modify: 11 });
+    });
+    it('should merge object and string', () => {
+        let x = 'hello';
+        let y = {x: 1};
+        let {merge, diffMap} = diff(x, y);
+        expect(merge).to.deep.equal('hello');
+        expect(diffMap).to.deep.equal({type: 'edit', modify: {x : 1} });
+    });
+    it('should merge object and array', () => {
+        let x = [1, 2, 3];
+        let y = {x: 1};
+        let {merge, diffMap} = diff(x, y);
+        let res = [1, 2, 3];
+        res.x = 1;
+        expect(merge).to.deep.equal(res);
+        expect(diffMap).to.deep.equal({ 
+          '0': { type: 'delete' },
+          '1': { type: 'delete' },
+          '2': { type: 'delete' },
+          'x': { type: 'add' } 
+        });
     });
     it('should merge json and get diff path map', () => {
         let x = [{
